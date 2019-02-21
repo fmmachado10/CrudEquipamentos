770e0f1132f338e58c6df32063af8943e34cf38e
fmmachado10 <fabricio.machado@tcm.go.gov.br>
2019-02-21 18:12:18 -0300

    criando pacotes classes

---
+++ src/main/java/modelo/Equipamento.java
@@ -0,0 +1,54 @@
+package modelo;
+
+import java.io.Serializable;
+
+public class Equipamento implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6445394253836413751L;
+
+	private long id;
+	
+	private String descricao;
+	
+	private String numeroPatrimonio;
+	
+	private String local;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getNumeroPatrimonio() {
+		return numeroPatrimonio;
+	}
+
+	public void setNumeroPatrimonio(String numeroPatrimonio) {
+		this.numeroPatrimonio = numeroPatrimonio;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	
+	
+
+}
---
+++ src/main/java/repositorio/EquipamentoDAO.java
@@ -0,0 +1,53 @@
+package repositorio;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import banco.DbUtil;
+import modelo.Equipamento;
+
+public class EquipamentoDAO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8288611855432346547L;
+	
+	 private Connection connection;
+
+	    public EquipamentoDAO() {
+				
+	        connection = DbUtil.getConnection();
+	        
+	    }
+
+	public void salvarEquipamento(Equipamento equipamento) {
+		
+		 try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("insert into equipamento(descricao, numeroPatrimonio, local) values (?, ?, ? )");
+	            
+	            preparedStatement.setString(1, equipamento.getDescricao());
+	            
+	            preparedStatement.setString(2, equipamento.getNumeroPatrimonio());
+	            
+	            preparedStatement.setString(4, equipamento.getLocal());
+	            
+	            preparedStatement.executeUpdate();
+
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+
+	}
+
+	public void excluirEquipamento(Equipamento equipamento) {
+
+	}
+
+	public void atualizarEquipamento(Equipamento equipamento) {
+
+	}
+
+}
