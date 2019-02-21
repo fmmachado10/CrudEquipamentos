4dfef5a1c4bbf528e08642db74286ed8565df7cc
fmmachado10 <fabricio.machado@tcm.go.gov.br>
2019-02-21 19:15:44 -0300

    métodos adicionados

--- src/main/java/servlet/CrudEquipamentosServlet.java
+++ src/main/java/servlet/CrudEquipamentosServlet.java
@@ -7,34 +7,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CrudEquipamentosServlet
- */
+
 @WebServlet("/CrudEquipamentosServlet")
 public class CrudEquipamentosServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
+   
     public CrudEquipamentosServlet() {
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		response.getWriter().append("Teste... ").append(request.getContextPath());
+		
+		  System.out.println("Funfs!!!");
+
+	        response.getWriter().append("<h1>OK Fabricio</h1>");	        
+	        
+	        System.out.println("xxx");
+	        
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
 		doGet(request, response);
+		
 	}
 
 }
